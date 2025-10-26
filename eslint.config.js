f6f724b3f61ac724bb4675204a6454afa3c8a62a
Manaf941 <manaf@2221.ch>
2025-10-26 16:44:40 +0100

    chore: fight with eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,35 +1,29 @@
-import js from "@eslint/js"
-import ts from "typescript-eslint"
+import eslint from "@eslint/js"
+import { defineConfig } from "eslint/config"
+import tseslint from "typescript-eslint"
+import stylistic from "@stylistic/eslint-plugin"
 
-export default [
+export default defineConfig(
     {
-        ignores: [
-            "dist/",
-            "node_modules/",
-        ],
+        ignores: ["dist/"],
     },
-    js.configs.recommended,
-    ...ts.configs.strict,
+    eslint.configs.recommended,
+    tseslint.configs.recommended,
+
+    {
+        plugins: {
+            "@stylistic/ts": stylistic,
+        },
+    },
+    tseslint.configs.stylistic,
+
     {
         rules: {
-            quotes: [
-                "error",
-                "double",
-                {
-                    avoidEscape: true,
-                    allowTemplateLiterals: true,
-                },
-            ],
-
-            // typescript already verifies this
+            // Managed by typescript
             "no-undef": "off",
-            "no-constant-condition": "error",
 
-            "@typescript-eslint/explicit-module-boundary-types": "off",
-            "@typescript-eslint/no-explicit-any": "off",
-            "@typescript-eslint/no-empty-function": "off",
-            "@typescript-eslint/no-var-requires": "off",
-            "@typescript-eslint/no-non-null-assertion": "off",
+            // This one was disabled, if it's useful, enable it back.
+            // "no-self-assign": "off",
 
             "no-empty": [
                 "error",
@@ -37,6 +31,21 @@ export default [
                     allowEmptyCatch: true,
                 },
             ],
+
+            // non-null assertion are useful when you know the
+            // value, such as when in a branch. Also, it's not
+            // always your false since this might be coming from
+            // a library.
+            "@typescript-eslint/no-non-null-assertion": "off",
+
+            "@stylistic/ts/quotes": [
+                "error",
+                "double",
+                {
+                    avoidEscape: true,
+                    allowTemplateLiterals: "always",
+                },
+            ],
         },
     },
-]
+)
